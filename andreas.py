6e6938e532fb6bd1a31b76ca6d7cd0f027ab8878
wirtzzz <pauperandreas@gmail.com>
2023-02-12 18:11:25 +0100

    modifications sur la fonction FitKataInGrid

--- andreas.py
+++ andreas.py
@@ -1,6 +1,34 @@
 from tkinter import *
 import copy
 #à partir d'ici c'est la merde
+#%% lesdits kataminos
+
+GrandL=[[1,1],[1,0],[1,0],[1,0]]
+GrandT=[[2,0],[2,2],[2,0],[2,0]]
+GrandEclair=[[3,0],[3,0],[3,3],[0,3]]
+
+GrandV=[[4,4,4],[4,0,0],[4,0,0]]
+
+NormalP=[[5,5],[5,5],[5,0]]
+NormalC=[[6,6],[6,0],[6,6]]
+
+BizarrdZ=[[7,0,0],[7,7,7],[0,0,7]]
+
+IBarre4=[[8],[8],[8],[8]]
+
+PetitL=[[9,9],[9,0],[9,0]]
+PetitT=[[10,0],[10,10],[10,0]]
+PetitEclair=[[11,0],[11,11],[0,11]]
+
+Carre=[[12,12],[12,12]]
+
+IBarre3=[[13],[13],[13]]
+PetitV=[[14,14],[14,0]]
+
+IBarre2=[[15],[15]]
+
+Point=[[16]]
+
 #%% maths et dessin
 def DrawGrid(Grid):             #afficher la grille complétée
     col={1:"#00f",2:"#0f0",3:"#f00",4:"#dd0",5:"#0ff",6:"#f0f",7:"#800",8:"#080",9:"#008",10:"#880",11:"#808",12:"#888",13:"#400",14:"#040",15:"#004",16:"#044"}
@@ -27,8 +55,6 @@ ws.mainloop()
 
 #%% tourner le kata
 Grid=[[0 for _ in range(13)] for _ in range(5)]
-Kata1=[[1,1,1],[0,1,0],[1,1,0]]
-Kata2=[[0,2],[0,2],[2,2]]
 def RotateKataClockwise(Kata):          #tourner la pièce dans le sens horaire, fonctionne
     newKata=[]
     for i in range(len(Kata[0])):
@@ -38,23 +64,37 @@ def RotateKataClockwise(Kata):          #tourner la pièce dans le sens horaire,
         newKata.append(kataL)
     return newKata
 
-def FitKataInGrid(Kata,G):              #fonction qui cherche à faire rentrer un kata dans une grille donnée
+def FlipKata(Kata):                     #retourner la pièce
+    newKata=[]
+    for i in range(len(Kata)):
+        kataL=[]
+        for j in range(len(Kata[0])):
+            kataL.append(Kata[i][len(Kata[0])-j-1])
+        newKata.append(kataL)
+    return newKata
+
+def FitKataInGrid(Kata,G,flip):              #fonction qui cherche à faire rentrer un kata dans une grille donnée
     newG=copy.deepcopy(G)
     for _ in range(4):
         u,v=0,0
         newKata=copy.deepcopy(Kata)
         while u+len(newKata)<=len(newG):
-            while v+len(newKata[0]):
+            while v+len(newKata[0])<=len(newG[0]):
                 newG=copy.deepcopy(G)
                 fitting=True
                 for i in range(len(newKata)):
                     for j in range(len(Kata[0])):
                         if newG[i+u][j+v]==0 or newKata[i][j]==0:               #ajoute le morceau de Kata dans le cas où la case est vide
                             newG[i+u][j+v]+=newKata[i][j]
+                            print(newG)
                         else:
                            fitting=False
                 if fitting:
                     return newG
                 v+=1
             u+=1
+            v=0
         newKata=RotateKataClockwise(newKata)
+    if flip:    
+        return FitKataInGrid(FlipKata(Kata), G, False)
+    return G
